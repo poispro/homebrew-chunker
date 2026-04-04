57c4b6afadd06678d02472731f5389cfe9d7a952
poispro <hi.podo.po@gmail.com>
2026-04-04 14:52:06 +0200

    Update chunker.rb

--- Formula/chunker.rb
+++ Formula/chunker.rb
@@ -1,8 +1,8 @@
 class Chunker < Formula
   desc "A script to slice large apps/files into chunks of your desired size for Discord and other sites!"
   homepage "https://github.com/poispro/chunker"
-  url "https://github.com/poispro/chunker/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "684674c787593ff9d41549bd076564977a54e9cbf01d4ef90207b5aad995ec44"
+  url "https://github.com/poispro/chunker/archive/refs/tags/v2.1.1.tar.gz"
+  sha256 "561cefed11766a1051bf3492b8c11d197ed95b3ed7edeb9dde6f041648c7cb76"
   license "MIT"
 
   def install
